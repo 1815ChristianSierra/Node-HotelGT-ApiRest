59b6c8637a58574456352b5b65433b6d0cf83e44
Christian Sierra <161896561+1815ChristianSierra@users.noreply.github.com>
2026-05-28 18:33:48 -0600

    Update database configuration to use environment variables
    modificando para produccion

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,4 +1,15 @@
 const config = {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "csierrae1815_HotelLaravel",
+    password: process.env.DB_PASSWORD || "!l3$DrvYn3tg8Sgk",
+    database: process.env.DB_NAME || "csierrae1815_Hotel_Laravel",
+    port: process.env.DB_PORT || 3306
+};
+
+module.exports = config;
+
+
+/*const config = {
     host: "localhost",
     user: "root",
     password: "",
@@ -7,3 +18,4 @@ const config = {
 };
 
 module.exports = config;
+*/
