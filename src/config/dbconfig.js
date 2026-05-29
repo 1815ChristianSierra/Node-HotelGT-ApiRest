f38b038fa27c4e94a62d8b865e7a4fe85bf0bf26
Christian Sierra <161896561+1815ChristianSierra@users.noreply.github.com>
2026-05-28 18:28:55 -0600

    Agregando variables para Producción
    Arreglando Hotfix

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,4 +1,14 @@
 const config = {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "csierrae1815_HotelLaravel",
+    password: process.env.DB_PASSWORD || "!l3$DrvYn3tg8Sgk",
+    database: process.env.DB_NAME || "csierrae1815_Hotel_Laravel",
+    port: process.env.DB_PORT || 3306
+};
+
+module.exports = config;
+
+/*const config = {
     host: "localhost",
     user: "root",
     password: "",
@@ -6,4 +16,4 @@ const config = {
     port: 3306
 };
 
-module.exports = config;
+module.exports = config;*/
